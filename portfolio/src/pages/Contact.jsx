a316c92238e1aa9d8c536c5530b852548a38c91d
Rohitkashyp <ronaldobravo0001@gmail.com>
2025-04-26 10:42:35 -0700

    update api email url

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
-const API_URL= import.meta.env.VITE_API_URL;
+const API_URL= import.meta.env.VITE_EMAIL;
 
 
 
